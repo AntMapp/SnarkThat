b43dd3c0d30f9ce31e90602a4e5cdb049e7511fb
Ant_Mapp <71773545+AntMapp@users.noreply.github.com>
2022-08-19 07:07:11 +0100

    Add files via upload

---
+++ interface_update.py
@@ -0,0 +1,30 @@
+import argparse
+from snarkthat import SnarkThat
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers(dest='command')
+
+export = subparser.add_parser('export')
+prove = subparser.add_parser('prove')
+verify = subparser.add_parser('verify')
+
+export.add_argument('--r', type=str)
+prove.add_argument('--a', type=int, required=True)
+prove.add_argument('--b', type=int, required=True)
+verify.add_argument('--a', type=int, required=True)
+verify.add_argument('--b', type=int, required=True)
+
+
+args = parser.parse_args()
+if args.command == 'export':
+    snarkthat = SnarkThat()
+    print('R1CS: \n', snarkthat.r1cs)
+
+
+elif args.command == 'prove':
+    snarkthat = SnarkThat()
+    print('R1CS: \n', snarkthat.r1cs, '\n S =', snarkthat.arithmetic_circuit(args.a,args.b))
+    
+elif args.command == 'verify':
+    snarkthat = SnarkThat()
+    print(snarkthat.verify(args.a,args.b))
